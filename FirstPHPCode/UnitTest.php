bc9795bf932ac0d62dc187ab0254d2fa058cab9c
jianhuazhou <jianhuazhou@tamu.edu>
2016-04-26 17:23:08 -0500

    Initial Load

---
+++ FirstPHPCode/UnitTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * UnitTest short summary.
+ *
+ * UnitTest description.
+ *
+ * @version 1.0
+ * @author jzhou
+ */
+class UnitTest extends PHPUnit_Framework_TestCase
+{
+	function testBasic()
+	{
+		$this->fff();
+	}
+
+	function fff()
+	{
+		return 12.8 / 2.0;
+	}
+}
